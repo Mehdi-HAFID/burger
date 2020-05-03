66fa4ed1d1fdd392896f2f86696fc708cbffcee5
Mehdi HAFID <mehdi.hafid@outlook.com>
2020-05-03 17:07:33 +0000

    Creating a Burger in Burger.js

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import PropTypes from 'prop-types';
 
-import classes from './BurgerIngredient.css';
+import classes from './BurgerIngredient.module.css';
 
 class BurgerIngredient extends Component {
 	render() {
@@ -37,7 +37,7 @@ class BurgerIngredient extends Component {
 	}
 };
 
-BurgerIngredient.prototype = {
+BurgerIngredient.propTypes = {
 	type: PropTypes.string.isRequired,
 }
 
