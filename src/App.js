c2927869924df0fdc52725ce01178ec426143704
Mehdi HAFID <mehdi.hafid@outlook.com>
2020-07-14 14:18:10 +0000

    Minor Improvements and Fixes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, {Component} from 'react';
-import {Route, Switch, withRouter, Redirect} from "react-router-dom";
+import React, {Component, Suspense} from 'react';
+import {Redirect, Route, Switch} from "react-router-dom";
 import {connect} from 'react-redux';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from "./containers/Checkout/Checkout";
-import Orders from "./containers/Orders/Orders";
-import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions/index';
+import Spinner from './components/UI/Spinner/Spinner'
+
+const asyncCheckout = React.lazy(() => import('./containers/Checkout/Checkout'));
+
+const asyncOrders = React.lazy(() => import('./containers/Orders/Orders'));
+
+const asyncAuth = React.lazy(() => import('./containers/Auth/Auth'));
 
 class App extends Component {
 
@@ -18,21 +22,26 @@ class App extends Component {
 
 	render() {
 		let routes = (
-			<Switch>
-				<Route path = "/auth" component = {Auth}/>
-				<Route path = '/' exact component = {BurgerBuilder}/>
-				<Redirect to = "/"/>
-			</Switch>
-		);
-		if (this.props.isAuthenticated) {
-			routes = (
+			<Suspense fallback = {Spinner}>
 				<Switch>
-					<Route path = '/checkout' component = {Checkout}/>
-					<Route path = '/orders' component = {Orders}/>
-					<Route path = "/logout" component = {Logout}/>
+					<Route path = "/auth" component = {asyncAuth}/>
 					<Route path = '/' exact component = {BurgerBuilder}/>
 					<Redirect to = "/"/>
 				</Switch>
+			</Suspense>
+		);
+		if (this.props.isAuthenticated) {
+			routes = (
+				<Suspense fallback = {Spinner}>
+					<Switch>
+						<Route path = '/checkout' component = {asyncCheckout}/>
+						<Route path = '/orders' component = {asyncOrders}/>
+						<Route path = "/logout" component = {Logout}/>
+						<Route path = "/auth" component = {asyncAuth}/>
+						<Route path = '/' exact component = {BurgerBuilder}/>
+						<Redirect to = "/"/>
+					</Switch>
+				</Suspense>
 			);
 		}
 
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {Route, Redirect} from "react-router-dom";
+import {Redirect, Route} from "react-router-dom";
 import {connect} from "react-redux";
 
 import ContactData from "./ContactData/ContactData";
